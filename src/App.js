46b00f4b05e05bcf215ee52cdb2f68cc528d01c1
diannita <ingeniera.dianam@gmail.com>
2023-03-06 20:20:25 +1300

    Embedded JS in JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import * as React from "react";
 
+function getByTitle(title) {
+  return title;
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {getByTitle("world")}</h1>
+
+      <label htmlFor="search" className="b-search">
+        Search:{" "}
+      </label>
+      <input id="search" type="text"></input>
     </div>
   );
 }
