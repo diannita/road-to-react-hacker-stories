49d290b6b6572f9215bf06585e9718333bbec877
diannita <ingeniera.dianam@gmail.com>
2023-03-27 21:45:21 +1300

    props in react - how to use it

--- src/App.js
+++ src/App.js
@@ -1,108 +1,79 @@
 import * as React from "react";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-//function declaration
-// function List() {
-//   return (
-//     <ul>
-//       {list.map(function (item) {
-//         return (
-//           <li key={item.objectID}>
-//             <span>
-//               <a href="{item.url}">{item.title + `  `}</a>
-//             </span>
-//             <span>{item.author}</span>
-//             <span>{item.num_comments}</span>
-//             <span>{item.points}</span>
-//           </li>
-//         );
-//       })}
-//     </ul>
-//   );
-// }
-
-//function expression
-// let List = function() {
-//   return (
-//     <ul>
-//       {list.map(function (item) {
-//         return (
-//           <li key={item.objectID}>
-//             <span>
-//               <a href="{item.url}">{item.title + `  `}</a>
-//             </span>
-//             <span>{item.author}</span>
-//             <span>{item.num_comments}</span>
-//             <span>{item.points}</span>
-//           </li>
-//         );
-//       })}
-//     </ul>
-//   );
-// }
-
-//arrow funcyion expression
-let List = () => {
+function List(props) {
   return (
-    <ul>
-      {list.map(function (item) {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href="{item.url}">{item.title + `  `}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        );
-      })}
-    </ul>
+    <div>
+      <h2>{props.title}</h2>
+      <ul>
+        {props.list.map(function (item) {
+          return <Item item={item} />;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+const Item = (props) => {
+  return (
+    <li key={props.item.objectID}>
+      <span>
+        <a href="{props.item.url}">{props.item.title + `  `}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
   );
 };
 
 function Search() {
-  const handlerChange = (event) => {
-    console.log(event);
-    // return undefined;
-  };
-
-  const handleMouseOver = (event) => {
-    console.log(event);
-  };
-
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input
-        id="search"
-        type="text"
-        onChange={handlerChange}
-        onMouseOver={handleMouseOver}
-      />
+      <input id="search" type="text" />
     </div>
   );
 }
 
 function App() {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const javascriptLibraries = [
+    {
+      title: "jQuery",
+      url: "https://jquery.org",
+      author: "Jonh Resig",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Angular",
+      url: "https://angularjs.org",
+      author: "Google",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -110,7 +81,8 @@ function App() {
       <hr />
       <Search />
       <hr />
-      <List />
+      <List list={stories} title="React Ecosystem" />
+      <List list={javascriptLibraries} />
     </div>
   );
 }
